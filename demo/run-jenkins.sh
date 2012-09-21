1bc4ec8a7e7b0b1dae7bffb689410c9953b55998
Miki Tebeka <miki.tebeka@gmail.com>
2012-09-21 12:24:48 -0700

    download

--- demo/run-jenkins.sh
+++ demo/run-jenkins.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Run Jenkins as daemon on port 8000
+
+# You can download jenkins.war from http://mirrors.jenkins-ci.org/war/latest/jenkins.war
 
 java -jar jenkins.war \
     --logfile=${PWD}/jenkins.log \
