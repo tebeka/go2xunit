107d7afbdd592b94c9033f06958cbb1411cc86a3
Yukinari Toyota <xxseyxx@gmail.com>
2014-05-17 20:46:58 +0900

    Non-permissive test names since it is appended by go test with GOMAXPROCS

--- go2xunit_test.go
+++ go2xunit_test.go
@@ -321,8 +321,24 @@ func Test_parseBuildFailed(t *testing.T) {
 }
 
 func Test_nameWithNum(t *testing.T) {
-	_, err := loadGotest("data/gotest-num.out", t)
+	suites, err := loadGotest("data/gotest-num.out", t)
 	if err != nil {
 		t.Fatalf("didn't parse name with number")
+	}
+
+	count := 1
+	if len(suites) != count {
+		t.Fatalf("bad number of suites. expected %d got %d", count, len(suites))
+	}
+
+	suite := suites[0]
+	if len(suite.Tests) != 1 {
+		t.Fatalf("bad number of tests. expected %d got %d", 1, len(suite.Tests))
+	}
+
+	test := suite.Tests[0]
+	name := "TestBasic"
+	if test.Name != name {
+		t.Fatalf("bad test name %s != %s", test.Name, name)
 	}
 }
