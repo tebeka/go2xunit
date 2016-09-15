b2b85a0e82dcab0293b5dca574d8f8b811a4bac9
aeb <ami.bizamcher@gmail.com>
2016-09-16 00:53:27 +0300

    Extract Suite & Test objects..

---
+++ suite.go
@@ -0,0 +1,68 @@
+package main
+
+// Test data structure
+type Test struct {
+	Name, Time, Message string
+	Failed              bool
+	Skipped             bool
+	Passed              bool
+}
+
+// Suite of tests (found in some unit testing frameworks)
+type Suite struct {
+	Name   string
+	Time   string
+	Status string
+	Tests  []*Test
+}
+
+// NumFailed return number of failed tests in suite
+func (suite *Suite) NumFailed() int {
+	count := 0
+	for _, test := range suite.Tests {
+		if test.Failed {
+			count++
+		}
+	}
+
+	return count
+}
+
+// NumSkipped return number of skipped tests in suite
+func (suite *Suite) NumSkipped() int {
+	count := 0
+	for _, test := range suite.Tests {
+		if test.Skipped {
+			count++
+		}
+	}
+
+	return count
+}
+
+// NumPassed return number of passed tests in the suite
+func (suite *Suite) NumPassed() int {
+	count := 0
+	for _, test := range suite.Tests {
+		if test.Passed {
+			count++
+		}
+	}
+
+	return count
+}
+
+// Count return the number of tests in the suite
+func (suite *Suite) Count() int {
+	return len(suite.Tests)
+}
+
+// hasFailures return true is there's at least one failing test in the suite
+func hasFailures(suites []*Suite) bool {
+	for _, suite := range suites {
+		if suite.NumFailed() > 0 {
+			return true
+		}
+	}
+	return false
+}
