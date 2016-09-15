7acb99803b928299da17274b2a10308952a6bb2c
aeb <ami.bizamcher@gmail.com>
2016-09-16 01:55:04 +0300

    Reword test fail description.

--- suite_test.go
+++ suite_test.go
@@ -7,7 +7,7 @@ func Test_NumFailed(t *testing.T) {
 		Tests: []*Test{},
 	}
 	if suite.NumFailed() != 0 {
-		t.Fatal("found more than 1 failure in empty list")
+		t.Fatal("Expected 0 failures, got:", suite.NumFailed())
 	}
 
 	suite.Tests = []*Test{
