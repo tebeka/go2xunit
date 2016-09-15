2548353b656f5e7d617da9fef96f401c0c3026e2
aeb <ami.bizamcher@gmail.com>
2016-09-16 02:29:19 +0300

    Remove duplication in tests.

--- suite_test.go
+++ suite_test.go
@@ -2,15 +2,11 @@ package main
 
 import "testing"
 
-func Test_NumFailed_Empty(t *testing.T) {
+func Test_EmptySuite(t *testing.T) {
 	suite := Suite{}
 	if failures := suite.NumFailed(); failures != 0 {
 		t.Fatal("Expected 0 failures, got:", failures)
 	}
-}
-
-func Test_NumSkipped_Empty(t *testing.T) {
-	suite := Suite{}
 	if skipped := suite.NumSkipped(); skipped != 0 {
 		t.Fatal("Expected 0 skipped, got:", skipped)
 	}
@@ -19,7 +15,7 @@ func Test_NumSkipped_Empty(t *testing.T) {
 func Test_NumFailed_Mixed(t *testing.T) {
 	suite := &Suite{
 		Tests: []*Test{
-			&Test{Failed: false},
+			&Test{Skipped: true},
 			&Test{Failed: true},
 			&Test{Failed: false},
 		},
@@ -28,17 +24,6 @@ func Test_NumFailed_Mixed(t *testing.T) {
 	if failures := suite.NumFailed(); failures != 1 {
 		t.Fatal("Expected 1 failures, got:", failures)
 	}
-}
-
-func Test_NumSkipped_Mixed(t *testing.T) {
-	suite := &Suite{
-		Tests: []*Test{
-			&Test{Failed: false},
-			&Test{Skipped: true},
-			&Test{Failed: false},
-		},
-	}
-
 	if skipped := suite.NumSkipped(); skipped != 1 {
 		t.Fatal("Expected 1 skipped, got:", skipped)
 	}
