24626d66f8c4eab19aabb2b76c1baaeca6930d48
aeb <ami.bizamcher@gmail.com>
2016-09-16 02:05:05 +0300

    Reorder tests in file.

--- suite_test.go
+++ suite_test.go
@@ -2,6 +2,13 @@ package main
 
 import "testing"
 
+func Test_NumFailed_Empty(t *testing.T) {
+	suite := Suite{}
+	if failures := suite.NumFailed(); failures != 0 {
+		t.Fatal("Expected 0 failures, got:", failures)
+	}
+}
+
 func Test_NumFailed_Mixed(t *testing.T) {
 	suite := &Suite{
 		Tests: []*Test{
@@ -13,12 +20,5 @@ func Test_NumFailed_Mixed(t *testing.T) {
 
 	if suite.NumFailed() != 1 {
 		t.Fatal("can't count")
-	}
-}
-
-func Test_NumFailed_Empty(t *testing.T) {
-	suite := Suite{}
-	if failures := suite.NumFailed(); failures != 0 {
-		t.Fatal("Expected 0 failures, got:", failures)
 	}
 }
