dc390955765b7060de1c43807cbd7d9d29f6ecd7
aeb <ami.bizamcher@gmail.com>
2016-09-16 01:59:06 +0300

    Extract test.

--- suite_test.go
+++ suite_test.go
@@ -4,19 +4,23 @@ import "testing"
 
 func Test_NumFailed(t *testing.T) {
 	suite := &Suite{
-		Tests: []*Test{},
-	}
-	if failures := suite.NumFailed(); failures != 0 {
-		t.Fatal("Expected 0 failures, got:", failures)
-	}
-
-	suite.Tests = []*Test{
-		&Test{Failed: false},
-		&Test{Failed: true},
-		&Test{Failed: false},
+		Tests: []*Test{
+			&Test{Failed: false},
+			&Test{Failed: true},
+			&Test{Failed: false},
+		},
 	}
 
 	if suite.NumFailed() != 1 {
 		t.Fatal("can't count")
+	}
+}
+
+func Test_NumFailed_Empty(t *testing.T) {
+	suite := &Suite{
+		Tests: []*Test{},
+	}
+	if failures := suite.NumFailed(); failures != 0 {
+		t.Fatal("Expected 0 failures, got:", failures)
 	}
 }
