132733f65ba8d5176304b8e52239983b1418e169
aeb <ami.bizamcher@gmail.com>
2016-09-16 01:59:55 +0300

    Refactor empty suite declaration.

--- suite_test.go
+++ suite_test.go
@@ -17,9 +17,7 @@ func Test_NumFailed(t *testing.T) {
 }
 
 func Test_NumFailed_Empty(t *testing.T) {
-	suite := &Suite{
-		Tests: []*Test{},
-	}
+	suite := Suite{}
 	if failures := suite.NumFailed(); failures != 0 {
 		t.Fatal("Expected 0 failures, got:", failures)
 	}
