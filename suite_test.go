2d23d6f74372dfbca3b9908df83e68fd84483ab3
aeb <ami.bizamcher@gmail.com>
2016-09-16 17:09:00 +0300

    Refactor tests to use subtests.

--- suite_test.go
+++ suite_test.go
@@ -2,23 +2,31 @@ package main
 
 import "testing"
 
-func Test_EmptySuite(t *testing.T) {
+func TestEmptySuite(t *testing.T) {
 	suite := Suite{}
-	if count := suite.Count(); count != 0 {
-		t.Fatal("Expected 0 tests; got:", count)
-	}
-	if failures := suite.NumFailed(); failures != 0 {
-		t.Fatal("Expected 0 failures, got:", failures)
-	}
-	if skipped := suite.NumSkipped(); skipped != 0 {
-		t.Fatal("Expected 0 skipped, got:", skipped)
-	}
-	if passed := suite.NumPassed(); passed != 0 {
-		t.Fatal("Expected 0 passed, got:", passed)
-	}
+	t.Run("NumberOfTests", func(t *testing.T) {
+		if count := suite.Count(); count != 0 {
+			t.Fatal("Expected 0 tests; got:", count)
+		}
+	})
+	t.Run("NumberOfFailed", func(t *testing.T) {
+		if failures := suite.NumFailed(); failures != 0 {
+			t.Fatal("Expected 0 failures, got:", failures)
+		}
+	})
+	t.Run("NumberOfSkipped", func(t *testing.T) {
+		if skipped := suite.NumSkipped(); skipped != 0 {
+			t.Fatal("Expected 0 skipped, got:", skipped)
+		}
+	})
+	t.Run("NumberOfPassed", func(t *testing.T) {
+		if passed := suite.NumPassed(); passed != 0 {
+			t.Fatal("Expected 0 passed, got:", passed)
+		}
+	})
 }
 
-func Test_NumFailed_Mixed(t *testing.T) {
+func TestMixedSuite(t *testing.T) {
 	suite := &Suite{
 		Tests: []*Test{
 			&Test{Skipped: false},
@@ -29,40 +37,48 @@ func Test_NumFailed_Mixed(t *testing.T) {
 			&Test{Passed: true},
 		},
 	}
-
-	if count := suite.Count(); count != 6 {
-		t.Fatal("Expected 6 tests; got:", count)
-	}
-	if failures := suite.NumFailed(); failures != 1 {
-		t.Fatal("Expected 1 failures, got:", failures)
-	}
-	if skipped := suite.NumSkipped(); skipped != 1 {
-		t.Fatal("Expected 1 skipped, got:", skipped)
-	}
-	if passed := suite.NumPassed(); passed != 1 {
-		t.Fatal("Expected 1 passed, got:", passed)
-	}
+	t.Run("NumberOfTests", func(t *testing.T) {
+		if count := suite.Count(); count != 6 {
+			t.Fatal("Expected 6 tests; got:", count)
+		}
+	})
+	t.Run("NumberOfFailed", func(t *testing.T) {
+		if failures := suite.NumFailed(); failures != 1 {
+			t.Fatal("Expected 1 failures, got:", failures)
+		}
+	})
+	t.Run("NumberOfSkipped", func(t *testing.T) {
+		if skipped := suite.NumSkipped(); skipped != 1 {
+			t.Fatal("Expected 1 skipped, got:", skipped)
+		}
+	})
+	t.Run("NumberOfPassed", func(t *testing.T) {
+		if passed := suite.NumPassed(); passed != 1 {
+			t.Fatal("Expected 1 passed, got:", passed)
+		}
+	})
 }
 
-func TestMultipleSuitsWithoutFailures(t *testing.T) {
-	suites := []*Suite{
-		&Suite{},
-		&Suite{Tests: []*Test{&Test{Passed: true}}},
-		&Suite{Tests: []*Test{&Test{Skipped: true}}},
-	}
-	if hasFailures(suites) {
-		t.Fatal("Expected false, got: true")
-	}
-}
-
-func TestMultipleSuitsWithFailures(t *testing.T) {
-	suites := []*Suite{
-		&Suite{},
-		&Suite{Tests: []*Test{&Test{Passed: true}}},
-		&Suite{Tests: []*Test{&Test{Skipped: true}}},
-		&Suite{Tests: []*Test{&Test{Failed: true}}},
-	}
-	if !hasFailures(suites) {
-		t.Fatal("Expected true, got: false")
-	}
+func TestMultipleSuits(t *testing.T) {
+	t.Run("WithoutFailures", func(t *testing.T) {
+		suites := []*Suite{
+			&Suite{},
+			&Suite{Tests: []*Test{&Test{Passed: true}}},
+			&Suite{Tests: []*Test{&Test{Skipped: true}}},
+		}
+		if hasFailures(suites) {
+			t.Fatal("Expected false, got: true")
+		}
+	})
+	t.Run("WithFailures", func(t *testing.T) {
+		suites := []*Suite{
+			&Suite{},
+			&Suite{Tests: []*Test{&Test{Passed: true}}},
+			&Suite{Tests: []*Test{&Test{Skipped: true}}},
+			&Suite{Tests: []*Test{&Test{Failed: true}}},
+		}
+		if !hasFailures(suites) {
+			t.Fatal("Expected true, got: false")
+		}
+	})
 }
